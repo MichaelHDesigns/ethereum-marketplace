ffef82e410f1e8cfb489596fff2b4add5c2a2a86
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2022-12-26 00:00:10 -0500

    Update Marketplace.sol

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -1,72 +1,102 @@
-pragma solidity ^0.5.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
 
-contract Marketplace {
-  string public name;
-  uint public productCount=0;
-  mapping(uint => Product) public products;
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-struct Product {
-  uint id;
-  string name;
-  uint price;
-  address payable owner;
-  bool purchased;
-}
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-event ProductCreated (
-  uint id,
-  string name,
-  uint price,
-  address payable owner,
-  bool purchased
-);
+import "hardhat/console.sol";
 
-event ProductPurchased (
-  uint id,
-  string name,
-  uint price,
-  address payable owner,
-  bool purchased
-);
-  constructor() public {
-    name = "Dapp University Marketplace";
-  }
+contract Marketplace is ReentrancyGuard {
 
-  function createProduct(string memory _name, uint _price) public {
-    //Require a name
-    require(bytes(_name).length > 0, "Enter a valid name");
-    //Requiere a valid price
-    require(_price > 0, "Enter a valid price");
-    //Increment product count
-    productCount++;
-    //Create the product
-    products[productCount] = Product(productCount, _name, _price, msg.sender, false);
-    //Trigger an event
-    emit ProductCreated(productCount, _name, _price, msg.sender, false);
-  }
+    // Variables
+    address payable public immutable feeAccount; // the account that receives fees
+    uint public immutable feePercent; // the fee percentage on sales 
+    uint public itemCount; 
 
-  function purchaseProduct(uint _id) public payable {
-    //Fetch the product and make a copy of it
-    Product memory _product = products[_id];
-    //Fetch the owner
-    address payable _seller = _product.owner;
-    //Make sure the product has valid id
-    require(_product.id > 0 && _product.id <= productCount, "Enter valid id");
-    //Require that there is enough Ether in the transaction
-    require(msg.value >= _product.price,"Transfer the correct amount");
-    //Require that the product has not been purchased already
-    require(!_product.purchased, "Product has been purchased");
-    //Require that the buyer is not the seller
-    require(msg.sender != _seller, "Buyer cannot be seller");
-    //Transfer ownership to the buyer
-    _product.owner = msg.sender;
-    //Mark as purchased
-    _product.purchased = true;
-    //Update the product
-    products[_id] = _product;
-    //Pay the seller by sending them Ether
-    address(_seller).transfer(msg.value);
-    //Trigger an event
-    emit ProductPurchased(productCount, _product.name, _product.price, msg.sender, true);
-  }
+    struct Item {
+        uint itemId;
+        IERC721 nft;
+        uint tokenId;
+        uint price;
+        address payable seller;
+        bool sold;
+    }
+
+    // itemId -> Item
+    mapping(uint => Item) public items;
+
+    event Offered(
+        uint itemId,
+        address indexed nft,
+        uint tokenId,
+        uint price,
+        address indexed seller
+    );
+    event Bought(
+        uint itemId,
+        address indexed nft,
+        uint tokenId,
+        uint price,
+        address indexed seller,
+        address indexed buyer
+    );
+
+    constructor(uint _feePercent) {
+        feeAccount = payable(msg.sender);
+        feePercent = _feePercent;
+    }
+
+    // Make item to offer on the marketplace
+    function makeItem(IERC721 _nft, uint _tokenId, uint _price) external nonReentrant {
+        require(_price > 0, "Price must be greater than zero");
+        // increment itemCount
+        itemCount ++;
+        // transfer nft
+        _nft.transferFrom(msg.sender, address(this), _tokenId);
+        // add new item to items mapping
+        items[itemCount] = Item (
+            itemCount,
+            _nft,
+            _tokenId,
+            _price,
+            payable(msg.sender),
+            false
+        );
+        // emit Offered event
+        emit Offered(
+            itemCount,
+            address(_nft),
+            _tokenId,
+            _price,
+            msg.sender
+        );
+    }
+
+    function purchaseItem(uint _itemId) external payable nonReentrant {
+        uint _totalPrice = getTotalPrice(_itemId);
+        Item storage item = items[_itemId];
+        require(_itemId > 0 && _itemId <= itemCount, "item doesn't exist");
+        require(msg.value >= _totalPrice, "not enough ether to cover item price and market fee");
+        require(!item.sold, "item already sold");
+        // pay seller and feeAccount
+        item.seller.transfer(item.price);
+        feeAccount.transfer(_totalPrice - item.price);
+        // update item to sold
+        item.sold = true;
+        // transfer nft to buyer
+        item.nft.transferFrom(address(this), msg.sender, item.tokenId);
+        // emit Bought event
+        emit Bought(
+            _itemId,
+            address(item.nft),
+            item.tokenId,
+            item.price,
+            item.seller,
+            msg.sender
+        );
+    }
+    function getTotalPrice(uint _itemId) view public returns(uint){
+        return((items[_itemId].price*(100 + feePercent))/100);
+    }
 }
