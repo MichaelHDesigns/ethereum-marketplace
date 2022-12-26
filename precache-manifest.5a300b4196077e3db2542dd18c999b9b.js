444d25278c8f2510528987c97442265538b196bd
MichaelHDesigns <devilking6105@gmail.com>
2022-12-26 02:25:44 +0000

    Updates

---
+++ precache-manifest.5a300b4196077e3db2542dd18c999b9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ace0b47d6ac6fb92191",
+    "url": "/ethereum-marketplace/static/css/main.41cf10c9.chunk.css"
+  },
+  {
+    "revision": "4ace0b47d6ac6fb92191",
+    "url": "/ethereum-marketplace/static/js/main.4ace0b47.chunk.js"
+  },
+  {
+    "revision": "02493663268e909a4446",
+    "url": "/ethereum-marketplace/static/css/1.21749cc4.chunk.css"
+  },
+  {
+    "revision": "02493663268e909a4446",
+    "url": "/ethereum-marketplace/static/js/1.02493663.chunk.js"
+  },
+  {
+    "revision": "42e5a1b95d82815e13c3",
+    "url": "/ethereum-marketplace/static/js/runtime~main.42e5a1b9.js"
+  },
+  {
+    "revision": "fafde5d07d53b90a9ba9eec0733289ab",
+    "url": "/ethereum-marketplace/index.html"
+  }
+];
