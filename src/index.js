d7cef3d785da451aff35ae2a1801087513cb0975
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2022-12-26 00:14:26 -0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css'
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Web3Provider from './store/Web3Provider';
+import CollectionProvider from './store/CollectionProvider';
+import MarketplaceProvider from './store/MarketplaceProvider';
+import App from './App';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <Web3Provider>
+    <CollectionProvider>
+      <MarketplaceProvider>
+        <App />
+      </MarketplaceProvider>
+    </CollectionProvider>
+  </Web3Provider>, 
+  document.getElementById('root')
+);
