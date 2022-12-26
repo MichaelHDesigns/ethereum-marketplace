9de00b283d607d01fb8a93a55998b6d9070fa3cc
MichaelHDesigns <devilking6105@gmail.com>
2022-12-26 05:53:08 +0000

    Updates

---
+++ precache-manifest.438b885db4b91032da7658c2338f1a99.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f27ad4d1a36633fbf9857355e0aeded0",
+    "url": "/ethereum-marketplace/index.html"
+  },
+  {
+    "revision": "8ffb7cfb0664e1e3e16f",
+    "url": "/ethereum-marketplace/static/css/2.1f023efc.chunk.css"
+  },
+  {
+    "revision": "8ffb7cfb0664e1e3e16f",
+    "url": "/ethereum-marketplace/static/js/2.37471146.chunk.js"
+  },
+  {
+    "revision": "1e13691fa9acd3f01459cc5ebc8739a2",
+    "url": "/ethereum-marketplace/static/js/2.37471146.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5b6c7bd542107b074695",
+    "url": "/ethereum-marketplace/static/js/main.5f27393a.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/ethereum-marketplace/static/js/main.5f27393a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1e9517a2203d45132281",
+    "url": "/ethereum-marketplace/static/js/runtime-main.9e157011.js"
+  },
+  {
+    "revision": "9de7f7f4218fabfeec56b3823a7c7bdf",
+    "url": "/ethereum-marketplace/static/media/logo2.9de7f7f4.PNG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ethereum-marketplace/precache-manifest.0ca27a6f2891e80d17cb73c007fdb82a.js"
+  "/ethereum-marketplace/precache-manifest.438b885db4b91032da7658c2338f1a99.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/ethereum-marketplace/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ethereum-marketplace/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
